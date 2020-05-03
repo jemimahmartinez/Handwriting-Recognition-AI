963dce52b6d86d355178d546e46974f149c0361f
Jemimah Martinez <martinezjemimah@gmail.com>
2020-05-03 21:22:45 +1200

    updated alexnet model

--- models/alexnet.py
+++ models/alexnet.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-import torch.nn.function as F
+import torch.nn.functional as F
 import torch
 
 # The network should inherit from the nn.module
@@ -29,25 +29,26 @@ class AlexNet(nn.Module):
 
     def forward(self, x):
         x = self.conv1(x)
-        x = nn.ReLu(x) # activation function
+        x = F.ReLu(x) # activation function
         x = self.maxP1
         x = self.conv2(x)
-        x = nn.ReLu(x) # activation function
+        x = F.ReLu(x) # activation function
         x = self.maxP2(x)
         x = self.conv3(x)
-        x = nn.ReLu(x)
+        x = F.ReLu(x)
         x = self.conv4(x)
-        x = nn.ReLu(x)
+        x = F.ReLu(x)
         x = self.conv5(x)
-        x = nn.ReLu(x)
+        x = F.ReLu(x)
         x = self.maxP3(x)
-        # do i need to flatten the array before the fully connected layer again??
+        x = torch.flatten(x, 1)
         x = self.fc1(x)
-        x = nn.ReLu(x)
+        x = F.ReLu(x)
         x = self.fc2(x)
-        x = nn.ReLu(x)
+        x = F.ReLu(x)
         x = self.fc3(x)
-        x = nn.Softmax(x, dim=1)
+        output = F.Softmax(x, dim=1)
+        return output
 
 
 
