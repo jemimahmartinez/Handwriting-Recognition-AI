12275c58cafbd70628cd9127ed7aaddbcb0207c8
Jemimah Martinez <martinezjemimah@gmail.com>
2020-05-04 20:00:30 +1200

    Added parameters

--- models/alexnet.py
+++ models/alexnet.py
@@ -9,23 +9,23 @@ class AlexNet(nn.Module):
 
         # Convolutional layers (5)
         # parameters(input channels, output channels, kernel size, stride)
-        self.conv1 = nn.Conv2d()
-        self.conv2 = nn.Conv2d()
-        self.conv3 = nn.Conv2d()
-        self.conv4 = nn.Conv2d()
-        self.conv5 = nn.Conv2d()
+        self.conv1 = nn.Conv2d(3, 64, 11, 4)
+        self.conv2 = nn.Conv2d(64, 192, 5, 2)
+        self.conv3 = nn.Conv2d(192, 384, 3, 1)
+        self.conv4 = nn.Conv2d(384, 256, 3, 1)
+        self.conv5 = nn.Conv2d(256, 256, 3, 1)
 
         # Max Pooling
         # paramters(kernel size)
-        self.maxP1 = nn.MaxPool2d()
-        self.maxP2 = nn.MaxPool2d()
-        self.maxP3 = nn.MaxPool2d()
+        self.maxP1 = nn.MaxPool2d(3)
+        self.maxP2 = nn.MaxPool2d(3)
+        self.maxP3 = nn.MaxPool2d(3)
 
         # Fully Connected layers (3)
         # parameters(input size, output size)
-        self.fc1 = nn.Linear()
-        self.fc2 = nn.Linear()
-        self.fc3 = nn.Linear()
+        self.fc1 = nn.Linear(9216, 4096)
+        self.fc2 = nn.Linear(4096, 4096)
+        self.fc3 = nn.Linear(4096, 369)
 
     def forward(self, x):
         x = self.conv1(x)
