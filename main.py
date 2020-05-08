0e6219d6e8775d8a2c3e7cbd82915a4e9d9883e0
Jemimah Martinez <martinezjemimah@gmail.com>
2020-05-08 21:36:57 +1200

    added accuracy graph

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import torch.optim as optim
 import torchvision
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
-#from models.lenet5 import LeNet5
-from models.vgg16 import VGG16
+from models.lenet5 import LeNet5
+#from models.vgg16 import VGG16
+#from models.alexnet import AlexNet
 import torch.nn.functional as F
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 #import hasy_tools
@@ -17,9 +18,9 @@ def imsave(img):
     npimg = img.numpy()
     npimg = (np.transpose(npimg, (1, 2, 0)) * 255).astype(np.uint8)
     im = Image.fromarray(npimg)
-    im.save("./results/your_file.jpeg")
+    im.save("./results/lenet5_img.jpeg")
 
-def train_vgg16(log_interval, model, device, train_loader, optimizer, epoch):
+def train_lenet(log_interval, model, device, train_loader, optimizer, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
@@ -30,7 +31,7 @@ def train_vgg16(log_interval, model, device, train_loader, optimizer, epoch):
         # forward + backward + optimize
         torch.cuda.empty_cache()
         output = model(data)
-        output = output.reshape(-1, 1419264)#4257792)
+        #output = output.reshape(-1, 369)#11808)#vgg 1419264)#4257792)
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
@@ -39,7 +40,9 @@ def train_vgg16(log_interval, model, device, train_loader, optimizer, epoch):
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
 
-def test(model, device, test_loader):
+
+
+def test(model, device, test_loader, epoch, accuracy_epoch):
     model.eval()
     test_loss = 0
     correct = 0
@@ -52,11 +55,15 @@ def test(model, device, test_loader):
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
 
+            accuracy_epoch[0, epoch] = 100. * correct / len(test_loader.dataset)
+            accuracy_epoch[1, epoch] = epoch + 1
+
     test_loss /= len(test_loader.dataset)
 
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
+
 
 def main():
     epoches = 10 #14
@@ -75,15 +82,20 @@ def main():
 
     # --- Load the data ---
     train_data = torchvision.datasets.ImageFolder(root='./ImageFolder/train', transform=transforms.Compose([
-        transforms.RandomResizedCrop(224),
+        # transforms.RandomResizedCrop(224),
         transforms.ToTensor()
     ]))
-    train_loader = torch.utils.data.DataLoader(train_data, batch_size=1, shuffle=True, **kwargs) #151410
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=1000, shuffle=True, **kwargs) #151410
     test_data = torchvision.datasets.ImageFolder(root='./ImageFolder/test', transform=transforms.Compose([
-        transforms.RandomResizedCrop(224),
+        # transforms.RandomResizedCrop(224),
         transforms.ToTensor()
     ]))
-    test_loader = torch.utils.data.DataLoader(test_data, batch_size=3, shuffle=True, **kwargs) #16823
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=369, shuffle=True, **kwargs) #16823
+
+    #--- read csv file to get labels ---#
+    # label_file = open('hasy-data-labels.csv', 'r')
+    # image_labels = label_file.read()
+    # print(image_labels, 'labels')
 
     # get some random training images
     dataiter = iter(train_loader)
@@ -92,18 +104,39 @@ def main():
     imsave(img)
 
     # Build network and run
-    model = VGG16().to(device)
+    model = LeNet5().to(device, dtype=torch.float).cuda() #model = VGG16().to(device)
     optimizer = optim.Adam(model.parameters(), lr=0.001)
     scheduler = StepLR(optimizer, step_size=1, gamma=gamma)
+
+    iterations = int(167863/1000) #train_size/batch_size
+    accuracy_epoch = np.empty([2, (epoches + 1)*iterations])
+
     for epoch in range(1, epoches + 1):
         torch.cuda.empty_cache()
-        train_vgg16(log_interval, model, device, train_loader, optimizer, epoch)
-        test(model, device, test_loader)
+        train_lenet(log_interval, model, device, train_loader, optimizer, epoch)
+        test(model, device, test_loader, epoch, accuracy_epoch)
         scheduler.step()
 
     if save_model:
         print('after save_model')
-        torch.save(model.state_dict(), "./results/hasyv2_vgg16.pt")
+        torch.save(model.state_dict(), "./results/hasyv2_lenet5.pt")
+
+    #--- graphs ---
+    graph_accuracy = plt.figure()
+    plt.plot(accuracy_epoch[1], accuracy_epoch[0], color='blue')
+    plt.xlabel('Number of epochs')
+    plt.ylabel('Accuracy')
+
+    graph_accuracy.show()
+    graph_accuracy.savefig('results/accuracy_lenet5.jpeg')
+
+    # graph_loss = plt.figure()
+    # plt.plot()
+    # plt.xlabel('Number of training examples')
+    # plt.ylabel('Loss')
+    #
+    # graph_loss.show()
+    # graph_loss.savefig('results/train_loss.jpeg')
 
 if __name__ == '__main__':
     main()
---
+++ models/alexnet.py
@@ -0,0 +1,52 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+#https://github.com/pytorch/vision/blob/master/torchvision/models/alexnet.py
+# The network should inherit from the nn.module
+class AlexNet(nn.Module):
+    def __init__(self):
+        super(AlexNet, self).__init__()
+
+        # Convolutional layers (5)
+        # parameters(input channels, output channels, kernel size, stride)
+        self.conv1 = nn.Conv2d(3, 64, 11)#, 4)
+        self.conv2 = nn.Conv2d(64, 192, 5)#, 2)
+        self.conv3 = nn.Conv2d(192, 384, 3)#, 1)
+        self.conv4 = nn.Conv2d(384, 256, 3)#, 1)
+        self.conv5 = nn.Conv2d(256, 256, 3)#, 1)
+
+        # Max Pooling
+        # paramters(kernel size)
+        self.maxP1 = nn.MaxPool2d(3)
+        self.maxP2 = nn.MaxPool2d(3)
+        self.maxP3 = nn.MaxPool2d(3)
+
+        # Fully Connected layers (3)
+        # parameters(input size, output size)
+        self.fc1 = nn.Linear(6400, 4096) #9216, 4096
+        self.fc2 = nn.Linear(4096, 4096)
+        self.fc3 = nn.Linear(4096, 369)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x) # activation function
+        x = self.maxP1(x)
+        x = self.conv2(x)
+        x = F.relu(x) # activation function
+        x = self.maxP2(x)
+        x = self.conv3(x)
+        x = F.relu(x) # activation function
+        x = self.conv4(x)
+        x = F.relu(x) # activation function
+        x = self.conv5(x)
+        x = F.relu(x) # activation function
+        x = self.maxP3(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x) # activation function
+        x = self.fc2(x)
+        x = F.relu(x)  # activation function
+        x = self.fc3(x)
+        output = F.softmax(x, dim=1)
+        return output
